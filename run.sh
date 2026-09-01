6d1f834a5f7c386923927b71da434e0e159ef685
StardustMotion <44144837+StardustMotion@users.noreply.github.com>
2026-09-01 14:10:13 +0900

    Project setup / basic draw/hide

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Compile / build the .pk3 then boot it up in Zandronum
+# Change path below to your zandronum.exe !!
+pathZandronum="G:/games/megaman/8bdm/2023/3.3-alpha-r260112-1855\zandronum.exe"
+
+./acc/cbins.exe build
+
+$pathZandronum \
+-iwad "G:/games/megaman/8bdm/2023/megagame.wad" \
+-file "G:/games/megaman/8bdm/2023/MM8BDM-v6b.pk3" \
+-file "./acc/dist/megaUI-1.0.0.pk3" \
++sv_cheats 1
