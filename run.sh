a19f1881264efede744e28cba0b4106188f761fe
StardustMotion <44144837+StardustMotion@users.noreply.github.com>
2026-09-17 08:57:43 +0900

    Minigame 4 : weapon loadout + merge games in same file

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ pathZandronum="G:/games/megaman/8bdm/2023/3.3-alpha-r260112-1855\zandronum.exe"
 ./acc/cbins.exe build
 
 $pathZandronum \
--iwad "G:/games/megaman/8bdm/2023/megagame.wad" \
--file "G:/games/megaman/8bdm/2023/MM8BDM-v6b.pk3" \
+-iwad "G:/games/megaman/8bdm/2023/doom2.wad" \
 -file "./acc/dist/megaUI-1.0.0.pk3" \
 +sv_cheats 1
 
-# -iwad "G:/games/megaman/8bdm/2023/doom2.wad" \
+# -iwad "G:/games/megaman/8bdm/2023/megagame.wad" \
+# -file "G:/games/megaman/8bdm/2023/MM8BDM-v6b.pk3" \
 
